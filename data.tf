a7e0bec7e227dcab8035a4d5739b75e2d5c983bf
László Szalma <laszlo.szalma@proemergotech.com>
2018-08-31 11:21:13 +0200

    enabled fix for data

--- data.tf
+++ data.tf
@@ -1,7 +1,9 @@
 data "aws_vpc" "peer_from_vpc" {
+  count = "${var.enabled ? 1 : 0}"
   id = "${var.peer_from_vpc_id}"
 }
 
 data "aws_vpc" "peer_to_vpc" {
+  count = "${var.enabled ? 1 : 0}"
   id = "${var.peer_to_vpc_id}"
 }
