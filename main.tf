75536313a7f987481efc396c1a675ceeb92087bc
Norbert Csibra <norbert.csibra@proemergotech.com>
2018-10-03 13:06:51 +0200

    ...

--- main.tf
+++ main.tf
@@ -20,6 +20,16 @@ data "aws_vpc" "peer_to_vpc" {
   id       = "${var.peer_to_vpc_id}"
 }
 
+data "aws_route_tables" "peer_from_route_tables" {
+  provider = "aws.peer"
+  vpc_id   = "${var.peer_from_vpc_id}"
+}
+
+data "aws_route_tables" "peer_to_route_tables" {
+  provider = "aws.peer"
+  vpc_id   = "${var.peer_to_vpc_id}"
+}
+
 resource "aws_vpc_peering_connection" "peer_from_to_peer_to_vpc" {
   count       = "${var.enabled ? 1 : 0}"
   peer_vpc_id = "${var.peer_to_vpc_id}"
@@ -46,17 +56,17 @@ resource "aws_vpc_peering_connection_accepter" "peer" {
 
 resource "aws_route" "peer_from_to_peer_to" {
   #  count = "${length(var.peer_from_route_tables)}"
-  count = "${var.enabled ? var.peer_from_route_tables_count : 0}"
+  count = "${var.enabled ? "${length(data.aws_route_tables.peer_from_route_tables)}" : 0}"
 
-  route_table_id            = "${element(var.peer_from_route_tables, count.index)}"
-  destination_cidr_block    = "${var.peer_to_vpc_cidr}"
+  route_table_id            = "${element(data.aws_route_tables.peer_from_route_tables, count.index)}"
+  destination_cidr_block    = "${data.aws_vpc.peer_to_vpc.cidr_block}"
   vpc_peering_connection_id = "${aws_vpc_peering_connection.peer_from_to_peer_to_vpc.id}"
 }
 
 resource "aws_route" "peer_to_to_peer_from" {
-  count = "${var.enabled ? var.peer_to_route_tables_count : 0}"
+  count = "${var.enabled ? "${length(data.aws_route_tables.peer_to_route_tables)}" : 0}"
 
-  route_table_id            = "${element(var.peer_to_route_tables, count.index)}"
-  destination_cidr_block    = "${var.peer_from_vpc_cidr}"
+  route_table_id            = "${element(data.aws_route_tables.peer_to_route_tables, count.index)}"
+  destination_cidr_block    = "${data.aws_vpc.peer_from_vpc.cidr_block}"
   vpc_peering_connection_id = "${aws_vpc_peering_connection.peer_from_to_peer_to_vpc.id}"
 }
